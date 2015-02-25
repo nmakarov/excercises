4f9f5266f8e5f4537c37e02fc0603e841060a565
Nick Makarov <nmakarov@gmail.com>
2015-02-25 13:47:15 -0800

    craziness coersion

--- Craziness/yep.js
+++ Craziness/yep.js
@@ -33,4 +33,49 @@ describe ('Crazy take #1', function () {
     // now, let's use it. It doesn't evaluate to false!!!
     assert(b ? 'true' : 'false' !== 'false');
   });
+
+  it('integers, anyone?', function () {
+    var a = 0;
+    var b = new Number(0);
+    assert.equal(a == b, true);
+    assert.equal(!!a == !!b, false); // this is crazy!
+    assert.equal(!!a, false);
+    assert.equal(!!b, true);
+  });
+
+  it('converts anything to a string...', function () {
+    assert.equal('' + false, 'false');
+  });
+  it('  ...or a number', function () {
+    assert.equal(true + true, 2);
+    assert.equal(true << true, 2);
+  });
+
+  it('coersion madness', function () {
+    assert.equal( []  === 0, false); // yep, expected, right?
+    assert.equal(+[]  === 0, true);  // wow
+    assert.equal(+[8] === 8, true);  // wow
+  });
+
+  it('valueOf crap', function () {
+    var valueOfCalled = false;
+    var O = function () {this.valueOf = function () {valueOfCalled = true; return false;};};
+    var o = new O();
+
+    // initially, no coersion happened:
+    assert.equal(valueOfCalled, false);
+
+
+    // numeric coescion happens:
+    assert.equal(+o, false);
+    assert.equal(valueOfCalled, true);
+    valueOfCalled = false; // reset the flag
+
+    // and now no coersion!
+    assert.equal(!!o, true);
+    assert.equal(valueOfCalled, false);
+
+    // in other words,
+    assert.equal(!!o !== +0, true);
+  });
 });
