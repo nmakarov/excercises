f97820fa56777ef123c3c7976b837082fdf24f95
Nick Makarov <nmakarov@gmail.com>
2015-03-26 11:11:07 -0700

    numbers formatter string added

--- Numbers/format.js
+++ Numbers/format.js
@@ -1,3 +1,8 @@
+/*
+ * These excersizes train different types of programmatic thinking - regexp thinking, procedural thinking, etc.
+ */
+
+
 var assert = require('assert');
 
 var numbers = [
@@ -7,15 +12,50 @@ var numbers = [
   [1234567890.23, '1,234,567,890.23'],
 ];
 
-var fmt = function (number) {
-  return (''+number).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-};
-
 describe('Formatting numbers', function () {
-  it('Integers, regexp', function () {
+
+  it('regexp', function () {
+
+    var fmt = function (number) {
+      return (''+number).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    };
 
     numbers.forEach(function (pair) {
       assert.equal(fmt(pair[0]), pair[1]);
     });
+  });
+
+
+
+  it('Pure string manipulations', function () {
+
+    var fmt = function (number) {
+
+      // massage input
+      number = '' + number;
+
+      var dotPosition = number.indexOf('.') !== -1 ? number.indexOf('.') : 999999;
+      var whole = number.substr(0, dotPosition);
+      var fraction = number.substr(dotPosition + 1);
+      var result = '';
+
+      for (var i = whole.length; i--;) {
+        result = whole[i] + result;
+        if ((whole.length - i) % 3 === 0 && i > 0) {
+          result = ',' + result;
+        }
+      }
+
+      if (fraction) {
+        result = result + '.' + fraction;
+      }
+
+      return result;
+    };
+
+    numbers.forEach(function (pair) {
+      assert.equal(fmt(pair[0]), pair[1]);
+    });
+
   });
 });
