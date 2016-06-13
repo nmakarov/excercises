dac3c1bfae038f2a22a566bec8a229efe8fa7d8b
Nick Makarov <nmakarov@gmail.com>
2016-06-13 14:15:55 -0700

    cleanup

--- Functions/trampoline.js
+++ Functions/trampoline.js
@@ -11,33 +11,30 @@ describe ("reqursions and trampolines", function () {
 	});
 
 	it("trampoline", function () {
-		var thunk = function (f, lst) {
-			return {tag: "thunk", func: f, args: lst};
+		var thunk = function (func, args) {
+			return {tag: "thunk", func: func, args: args};
 		};
 		var identity = function (x) {
-			return {tag: "value", val: x};
+			return {tag: "value", value: x};
 		};
-		var factorial = function (n, cont) {
-			if (n < 2) {
-				return thunk(cont, [n]);
-			} else {
-				var new_cont = function (v) {
-					var result = v * n;
-					return thunk(cont, [result]);
-				};
-				return thunk(factorial, [n-1, new_cont]);
-			}
-		};
-
 		var trampoline = function (thk) {
 			while (true) {
 				if (thk.tag === "value") {
-					return thk.val;
+					return thk.value;
 				} else {
 					thk = thk.func.apply(null, thk.args);
 				}
 			}
 		}
+
+		var factorial = function (current, done) {
+				return current < 2
+					? thunk(done, [current])
+					: thunk(factorial, [current-1, function (running) {
+						return thunk(done, [running * current]);
+					}]);
+		};
+
 
 		expect(trampoline(factorial(5, identity))).to.be.equal(120);
 
